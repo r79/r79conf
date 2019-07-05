5d1110cf5aea668df882c201dffcd37aaf0e5394
r79 <r@r79.io>
2019-07-05 21:09:41 +0200

    fixed links.sh and actually made it configurable (jesus christ I used PATH as my var in there I should stop coding goooodddddd)

--- links.sh
+++ links.sh
@@ -3,7 +3,7 @@
 #############
 #CHANGE THIS#
 #############
-PATH=`pwd`/conf
+configpath=`pwd`/conf
 
 #############
 #PICK WISELY#
@@ -20,34 +20,38 @@ PATH=`pwd`/conf
 
 #set xorg keyboardmap
 sudo rm /etc/X11/xorg.conf.d/00-keyboard.conf
-sudo ln -s ~/r79conf/conf/xorg-keyboard-conf /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo ln -s $configpath/xorg-keyboard-conf /etc/X11/xorg.conf.d/00-keyboard.conf
 
 #dhcpcd speedhack
 sudo rm /etc/dhcpcd.conf
-sudo ln -s ~/r79conf/conf/dhcpcd.conf /etc/dhcpcd.conf
+sudo ln -s $configpath/dhcpcd.conf /etc/dhcpcd.conf
 
 #i3
 rm ~/.config/i3/config
-ln -s ~/r79conf/conf/i3-wm.conf ~/.config/i3/config
+ln -s $configpath/i3-wm.conf ~/.config/i3/config
 
 #zsh
 rm ~/.zshrc
-ln -s ~/r79conf/conf/.zshrc ~/.zshrc
+ln -s $configpath/.zshrc ~/.zshrc
+
+#bashrc
+rm ~/.bashrc
+ln -s $configpath/.bashrc ~/.bashrc
 
 #xfce4-terminal
 rm -r ~/.config/xfce4/terminal
-ln -s ~/r79conf/conf/terminal ~/.config/xfce4/terminal
+ln -s $configpath/terminal ~/.config/xfce4/terminal
 
 #i3status
 #ignoring some errors here as they might happen if one runs the script another time
 mkdir ~/.config/i3status
 rm ~/.config/i3status/config
-ln -s ~/r79conf/conf/i3status.conf ~/.config/i3status/config
+ln -s $configpath/i3status.conf ~/.config/i3status/config
 
 #xinitrc
 rm ~/.xinitrc
-ln -s ~/r79conf/conf/.xinitrc ~/.xinitrc
+ln -s $configpath/.xinitrc ~/.xinitrc
 
 #pikaur
 rm ~/.config/pikaur.conf
-ln -s ~/r79conf/conf/pikaur.conf ~/.config/pikaur.conf
+ln -s $configpath/pikaur.conf ~/.config/pikaur.conf
